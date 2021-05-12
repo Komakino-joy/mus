cca2aaa3e365cd70d9b020e5a16f6abb1a3c015d
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-05-11 20:44:26 -0700

    initial commit: skeleton of homepage, added node sass styling to homepage

---
+++ mus/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Homepage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <Homepage></Homepage>
+    </div>
+  );
+}
+
+export default App;
