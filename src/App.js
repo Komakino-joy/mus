698c09b8cfaa58046df53d76e41aef919ef5815a
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-05-13 21:19:43 -0700

    moved data to firestore

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
-function App({ setCurrentUser, currentUser }) {
+function App({ setCurrentUser, currentUser}) {
   //TODO: sets persistent user session for firebase.
   //TODO: this is an open subscription to firebase, connection is always open when app is mounted on DOM.
   //TODO: close subscription on Unmount
